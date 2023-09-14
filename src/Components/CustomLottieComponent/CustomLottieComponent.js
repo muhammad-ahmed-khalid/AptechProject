c8dfc42e2a3895bb3a4ca64696d255478e3e1905
Ahmed Khalid <ahmed.khalid@koderlabs.com>
2023-09-15 01:58:57 +0500

    Home Screen Done With Lottie Animation

--- src/Components/CustomLottieComponent/CustomLottieComponent.js
+++ src/Components/CustomLottieComponent/CustomLottieComponent.js
@@ -1,31 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import React from 'react';
 import LottieView from 'lottie-react-native';
-import Home from '../../Container/AppContainer/Home/Home';
 
-const CustomLottieComponent =  ({ text = 'No Record Found' }) => {
+const CustomLottieComponent = ({text = 'No Record Found', isAddEvent = false, handlePress}) => {
+  console.log("AHMED")
   return (
-    <View style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={() => handlePress()} onLongPress={() => console.log("Long Pressed")}>
       <LottieView
         style={styles.lottieStyle}
         resizeMode="cover"
         autoPlay={true}
         loop={true}
-        source={require('../../assets/Lottie/nothingFound.json')}
+        progress={0}
+        source={isAddEvent ? require('../../assets/Lottie/animation_lmjmi29x.json') : require('../../assets/Lottie/ViewEvents.json')}
       />
       <Text style={styles.textStyle}>{text}</Text>
-      
-    </View>
-  )
-}
+    </TouchableOpacity>
+  );
+};
 
-export default React.memo(CustomLottieComponent)
+export default React.memo(CustomLottieComponent);
 const styles = StyleSheet.create({
-    container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
-    textStyle: {
-      marginBottom: 10,
-    //   ...Fonts.SemiBold(),
-    },
-    lottieStyle: { height:300 },
-  });
-  
+  container: {
+    height: 200,
+    width: '46%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: 'grey',
+    borderRadius: 12
+  },
+  textStyle: {
+    marginBottom: 10,
+    color: '#000',
+    fontSize: 14
+  },
+  lottieStyle: {height: 70, width: 100},
+});
--- src/Container/AppContainer/Home/Home.jsx
+++ src/Container/AppContainer/Home/Home.jsx
@@ -1,11 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Image } from 'react-native'
 import React from 'react'
 import CustomLottieComponent from '../../../Components/CustomLottieComponent/CustomLottieComponent'
-
 const Home = () => {
+  const handlePressAddEvent = () => {
+    console.log("Add Event")
+  }
+  const handlePressViewEvents = () => {
+    console.log("handlePress View Event")
+  }
   return (
     <View style={styles.root}>
-      <CustomLottieComponent />
+      <View style={styles.innerContainer}>
+           <CustomLottieComponent text='Add Event' isAddEvent={true} handlePress={handlePressAddEvent}/>  
+           <CustomLottieComponent text='View Event' handlePress ={handlePressViewEvents} />  
+      </View>
     </View>
   )
 }
@@ -15,5 +23,16 @@ export default Home
 const styles = StyleSheet.create({
     root:{
         flex: 1,
-    }
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    innerContainer:{
+      flexDirection: 'row',
+      height: '100%',
+      width: '100%',
+      alignItems: 'center',
+      justifyContent: 'center',
+      paddingHorizontal: 20
+    },
+    lottieStyle: {height: 300},
 })
